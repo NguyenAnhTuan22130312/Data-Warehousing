d7003f553fb4ec24090f6a151d109beca52a6e55
tuandev <22130313@st.hcmuaf.edu.vn>
2025-11-09 11:13:03 +0700

    update-extract

--- scripts/etl_main.py
+++ scripts/etl_main.py
@@ -2,11 +2,18 @@ import os
 import datetime
 from configparser import ConfigParser
 import sys, os
+import argparse
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from utils.db_utils import connect_db, insert_log, update_log_status
+from utils.db_utils import connect_db, insert_log, update_log_status, get_log_by_id
 from utils.email_utils import send_email
 
+
+
+parser = argparse.ArgumentParser(description="ETL Player Data - Manual or Scheduled")
+parser.add_argument('--manual', action='store_true', help='Chạy thủ công')
+parser.add_argument('--date', type=str, help='Ngày chạy ETL (YYYY-MM-DD), mặc định hôm nay')
+args = parser.parse_args()
 
 job_name = "ETL_Player_Data"
 start_time = datetime.datetime.now()
@@ -14,17 +21,35 @@ log_id = None
 conn = None
 cursor = None
 
+
+# Xác định ngày chạy ETL
+run_date = args.date or datetime.datetime.now().strftime("%Y-%m-%d")
+print(f"Ngày chạy ETL: {run_date}")
+
 try:
     # 2. Gọi kết nối DB ControlManagementDB  trong file D:\Data-Warehousing\utils\db_utils.py
     conn, cursor = connect_db()
+
     # 4. Ghi lại log với trạng thái RUNNING để bắt đầu quá trình ETL
     log_id = insert_log(cursor, job_name, start_time, "RUNNING")
     conn.commit()
     print(f"✅ Scheduler started at {start_time}")
 
+    #Kiểm tra có trạng thái RUNNNG hay không ? 
+    check_log = get_log_by_id(cursor, log_id)
+    #chưa
+    if not check_log or check_log["status"] != "RUNNING":
+        raise Exception("❌ Không tìm thấy log RUNNING trong DB. Hủy tiến trình extract.")
+    
+    #Rồi
     # 5. Gọi script extract.py
-    print("Đang chạy file extract.py ...")
-    result_code = os.system("python scripts/extract.py")
+    print("Đang chạy extract.py ...")
+    extract_cmd = f"python scripts/extract.py --date {run_date}"
+    result_code = os.system(extract_cmd)
+
+    if result_code != 0:
+        raise Exception(f"extract.py thất bại với mã lỗi: {result_code}")
+    
 
     # 6.8 cập nhật lại log  quá trình ETL với trạng thái SUCCESS
     end_time = datetime.datetime.now()
@@ -44,9 +69,6 @@ try:
         body=f"Job ETL_Player_Data đã chạy thành công ✅\nThời gian: {start_time.strftime('%Y-%m-%d %H:%M:%S')}",
         to_list=receivers,
     )
-    # 7. Truy cập file transform.py để thực hiện Transform
-    print("Đang chạy file transform.py ...")
-    result_code = os.system("python scripts/transform.py")
 
 except Exception as e:
     # Cập nhật trạng thái quá trình ETL thành FAILED
